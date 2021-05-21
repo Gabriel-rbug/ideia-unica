efc88b0a7b857b4eafcba7ff393ff52c1b54c1e4
Gabriel <grgabrielribeiro9@gmail.com>
2021-05-21 17:21:53 -0300

    primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 function Home() {
-    return <div>Home</div>
+    return (
+    <>
+        <div>Home 1</div>
+        <div>Teste</div>
+        <Contador />
+        
+    </>
+    )
+           
+} 
+  
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+    function AdicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        
+<div>
+        <div>{contador}</div>
+        <button onClick={AdicionarContador}>Adicionar</button>
+        </div>
+    
+    )
 }
 
 export default Home
