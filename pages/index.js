6d3e320fd029ab032963f59d0efe771eeb0bbda6
Gabriel <grgabrielribeiro9@gmail.com>
2021-05-21 18:01:16 -0300

    Indexjs.2

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,38 @@ import { useState } from 'react';
 
 function Home() {
     return (
-    <>
-        <div>Home 1</div>
-        <div>Teste</div>
-        <Contador />
-        
-    </>
+        <>
+            <div>Home 1</div>
+            <div>Teste</div>
+            <Contador/>
+        </>
     )
-           
+               
 } 
   
 
 function Contador() {
     const [contador,setContador] = useState(1);
+    const [contadorNovo, setContadorNovo] = useState(2);
+
+    function addValor5() {
+        setContadorNovo(contadorNovo + 2);
+        
+
+    }
+
     function AdicionarContador() {
         setContador(contador + 1);
     }
-    return (
-        
-<div>
-        <div>{contador}</div>
-        <button onClick={AdicionarContador}>Adicionar</button>
+    return (       
+        <div>
+            <div>{contador}</div>
+            <button onClick={AdicionarContador}>Adicionar</button>
+            <div>{contadorNovo}</div>
+            <button onClick={addValor5}>Atualizar</button>
         </div>
     
+
     )
 }
 
